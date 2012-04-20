f4f84b9902bc37381e2e1a6b80b3b537a2345e42
Thomas Fini Hansen <xen@xen.dk>
2012-04-20 15:42:53 +0200

    Updated search_collection to new theming.

--- templates/ting/ting_search_collection.tpl.php
+++ templates/ting/ting_search_collection.tpl.php
@@ -13,23 +13,23 @@
   <?php endif; ?>
   <div class="item graybox-btns<?php print $picture?'':' nopicture'; ?>">
     <a href="<?php print $collection->url; ?>">
-      <h3><?php print $collection->title ?></h3>
-      <?php if ($collection->creators_string) : ?>
+      <h3><?php print $ting_title ?></h3>
+      <?php if ($ting_creators) : ?>
       <span class="creator">
-        <?php echo t('By %creator_name%', array('%creator_name%' => $collection->creators_string)) ?>
+        <?php echo t('By %creator_name%', array('%creator_name%' => implode(', ', $ting_creators))) ?>
       </span>
       <?php endif; ?>
-      <?php if ($collection->date) : ?>
+      <?php if ($ting_publication_date) : ?>
       <span class="publication_date">
-        (<?php echo $collection->date /* TODO: Improve date handling, localizations etc. */ ?>)
+        (<?php echo $ting_publication_date /* TODO: Improve date handling, localizations etc. */ ?>)
       </span>
       <?php endif; ?>
       <div class="types">
         <?php print $type_list; ?>
       </div>
-      <?php if ($collection->abstract) : ?>
+      <?php if ($ting_abstract) : ?>
       <p class="abstract">
-        <?php print check_plain($collection->abstract); ?>
+        <?php print check_plain($ting_abstract); ?>
       </p>
       <?php endif; ?>
     </a>
